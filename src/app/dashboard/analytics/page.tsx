486503e8a0852cd57d2486bf2c179e5990064256
DeeKayMA <100499813+DeeKayMA@users.noreply.github.com>
2025-06-02 12:14:04 +0100

    Added base content for analytics

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -5,7 +5,40 @@ export default function Analytics() {
     return (
        <div className="flex flex-col w-full">
           <Header title="Analytics" />
-          <h1 className="text-3xl  font-bold underline">Analytics Content</h1>
+
+          {/* TOP PRIORITY DATA */}
+
+          {/* Four stat cards, showing real-time counts: */}
+          <div>
+            <div>Total Applications</div>
+            <div>Interviews</div>
+            <div>Offers</div>
+            <div>Rejections</div>
+          </div>
+          {/* 2 column grids */}
+   
+          <div>Application Status Breakdown</div>
+          <div>Application to Offer Rate</div>
+          <div>Interview to Offer Rate</div>
+
+          <div>Applications submitted over time, weekly,monthly</div>
+          <div>Days since last application</div>
+
+
+          {/* MEDIUM PRIORITY DATA  + behind show more */}
+
+          <div>Application to Interview Rate</div>
+          <div>Top companies</div>
+          <div>Top Job Titles</div>
+          <div>Average Salary</div>
+
+          <div>Applications by Job Type</div>
+          <div>Applications by Work Type</div>
+          <div>Applications by Locations</div>
+          
+
+        
+
         </div>
     );
 }
