836b06bf09616c6af27f66e3ad67e1179f3e2411
DeeKayMA <100499813+DeeKayMA@users.noreply.github.com>
2025-06-12 17:46:04 +0100

    Made header sticky and white

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,18 +1,19 @@
 // Auth layout (e.g., sidebar, header)
 
 import AppSidebar from "../../components/AppSidebar/AppSidebar";
-import { SidebarProvider} from "@/components/ui/sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar";
 import Header from "@/components/Header/Header";
 
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-      <SidebarProvider>
-        <AppSidebar />
-        <main className="w-full flex-1 flex flex-col relative overflow-hidden">
-          <Header/>
-          {children}
-        </main>
-      </SidebarProvider>
-    )
-  }
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="w-full">
+      <Header />
+      <main className="w-full flex-1 flex flex-col relative overflow-hidden">
+        {children}
+      </main>
+      </div>
+    </SidebarProvider>
+  );
+}
