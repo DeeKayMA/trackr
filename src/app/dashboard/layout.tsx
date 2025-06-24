3b9eb50e7906bdd0f3d2f3b671a273562da8d3d2
DeeKayMA <100499813+DeeKayMA@users.noreply.github.com>
2025-06-24 06:58:23 +0100

    Fix (Header): moved header outside of div

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,8 +8,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <div className="w-full min-h-screen flex flex-col overflow-x-hidden ">
       <Header />
+      <div className="w-full min-h-screen flex flex-col overflow-x-hidden ">
+      
       <main className="w-full flex-1 flex flex-col overflow-y-auto ">
         {children}
       </main>
