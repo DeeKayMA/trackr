ae56d1e6cbc23f139e50a4c8675239ec6df94f5d
DeeKayMA <100499813+DeeKayMA@users.noreply.github.com>
2025-07-11 12:42:13 +0100

    Fix: Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
+
 
 
 const geistSans = Geist({
@@ -48,6 +50,7 @@ export default function RootLayout({
         <Toaster />
         </ThemeProvider>
       </body>
+      <Analytics/>
     </html>
   );
 }
